9c4dfd147459e72dfd64017dea02f877e171d8c6
Khanadian <iqbal.52@hotmail.com>
2024-08-09 23:51:30 -0400

    factored percentiles into profit and price changes

--- osrs.py
+++ osrs.py
@@ -24,14 +24,16 @@ r2 = requests.get(url2, headers=headers)
 
 df = pd.DataFrame(columns=['item', 'low', 'high', 'profit', "ROI", "limit", \
                            "potential", "cost", "5m volume", "1h volume",\
-                           "24h volume", "avg Low diff", "avg high diff", 'miss'])
+                           "24h volume", "avg low diff", "avg high diff", \
+                           "diff % change", 'miss'])
 df_avg = pd.DataFrame(columns=['item', 'timestamp', 'low', 'high', 'lovolume',\
                                'hivolume'])
                                
 if r2.ok:
     items = json.loads(r2.content)
     inv_items = {v: k for k,v in items.items()}
-    
+else:
+    print("r2 fail")
 if response.ok:
     data = json.loads(response.content)
 
@@ -40,14 +42,75 @@ if response.ok:
         for k in data[key]:
             if int(k) in inv_items.keys():
                 high = int(data[key][k]["high"])
-                tax = int(math.floor(high * 0.01))
                 low = int(data[key][k]["low"])
-                profit = high - low - tax - 2 #subtract 2 to play margins
-                roi = round(profit / high, 3)
                 
-                df.loc[int(k)] = [inv_items[int(k)], low, high, profit, roi, 1, \
-                                  0, 0, 0, 0, 0, "N/A", "N/A", 0]
+                df.loc[int(k)] = [inv_items[int(k)], low, high, 0, 0, 1, \
+                                  0, 0, 0, 0, 0, "N/A", "N/A", 0, 0]
+else:
+    print("r fail")
 
+r4 = requests.get(url4, headers=headers)
+if r4.ok:
+    output = json.loads(r4.content)
+    for data in output:
+        if type(output[data]) == int:
+            continue
+        for key in output[data]:
+            try:
+                hivolume = output[data][key]['highPriceVolume']
+                lovolume = output[data][key]['lowPriceVolume']                  
+                #ratio = round(math.log((hivolume * lovolume)/(df.at[int(key), "limit"]**2), 4)
+                df.at[int(key), "5m volume"] = round(math.log(hivolume*lovolume + 1), 2)
+            except:
+                print(key)
+else:
+    print("r4 fail")
+    
+for i in range(1, 18):
+    timestamp = str(round((int(time.time())-(300*i))/300)*300) #must be nearest 300)
+    url4a = url4 + "?timestamp="+timestamp
+    r4 = requests.get(url4a, headers=headers)
+    if r4.ok:
+        output = json.loads(r4.content)
+        for data in output:
+            if type(output[data]) == int:
+                continue
+            for key in output[data]:
+                try:
+                    hivolume = output[data][key]['highPriceVolume']
+                    lovolume = output[data][key]['lowPriceVolume']
+                    if hivolume == 0 or lovolume == 0:
+                        df.at[int(key), "miss"] += 1
+                    hiprice = output[data][key]['avgHighPrice']
+                    loprice = output[data][key]['avgLowPrice']
+
+                    df_avg.loc[key + "-"+timestamp] = [df.at[int(key),'item'], \
+                            timestamp, loprice, hiprice, lovolume, hivolume]
+                except:
+                    print(key)
+    else:
+        print(url4a)
+    time.sleep(1)
+
+print(df_avg)
+df_avg.to_csv('out2.csv', index=False)
+
+for ind in df.index:
+    temp_df = df_avg[df_avg.index.str.startswith(str(ind)+"-")]
+    low = temp_df["low"].quantile(0.4, interpolation='higher')
+    high = temp_df["high"].quantile(0.6, interpolation='lower')
+    try:
+        tax = int(math.floor(high * 0.01))
+    except:
+        tax = int(math.floor(df.at[ind, "high"] * 0.01))
+    profit = high - low - tax
+    #print(high, low, tax, profit)
+    df.at[ind, "profit"] = profit
+    df.at[ind, "ROI"] = round(profit / high, 3)
+    df.at[ind, "avg low diff"] =df.at[ind, "low"] - low 
+    df.at[ind, "avg high diff"] =df.at[ind, "high"] - high
+    df.at[ind, "diff % change"] = round((df.at[ind, "avg high diff"] \
+                - df.at[ind, "avg low diff"])/(2*high), 3)
                 
 r3 = requests.get(url3, headers=headers)
 
@@ -66,52 +129,8 @@ if r3.ok:
             df.at[item["id"], "limit"] = lim
         except:
             print(item["name"])
-
-
-r4 = requests.get(url4, headers=headers)
-if r4.ok:
-    output = json.loads(r4.content)
-    for data in output:
-        if type(output[data]) == int:
-            continue
-        for key in output[data]:
-            try:
-                hivolume = output[data][key]['highPriceVolume']
-                lovolume = output[data][key]['lowPriceVolume']                  
-                ratio = round((hivolume * lovolume)/(df.at[int(key), "limit"]**2), 4)
-                df.at[int(key), "5m volume"] = round(math.log(hivolume+1) * math.log(lovolume+1), 2)
-            except:
-                print(key)
-
-for i in range(1, 19):
-    timestamp = str(round((int(time.time())-(300*i))/300)*300) #must be nearest 300)
-    url4a = url4 + "?timestamp="+timestamp
-    r4 = requests.get(url4a, headers=headers)
-    if r4.ok:
-        output = json.loads(r4.content)
-        for data in output:
-            if type(output[data]) == int:
-                continue
-            for key in output[data]:
-                try:
-                    hivolume = output[data][key]['highPriceVolume']
-                    lovolume = output[data][key]['lowPriceVolume']
-                    if hivolume == 0 or lovolume == 0:
-                        df.at[int(key), "miss"] += 1
-                    hiprice = output[data][key]['avgHighPrice']
-                    loprice = output[data][key]['avgLowPrice']
-
-                    df_avg.loc[key + "-"+timestamp] = [df.at[int(key), \
-                                        'item'], timestamp, loprice, hiprice, \
-                                                        lovolume, hivolume]
-                except:
-                    print(key)
-    else:
-        print(url4a)
-    time.sleep(1)
-
-print(df_avg)
-
+else:
+    print("r3 fail")
                 
 r5 = requests.get(url5, headers=headers)
 
@@ -126,9 +145,9 @@ if r5.ok:
             hvolume = output[data][key]['highPriceVolume']
             lvolume = output[data][key]['lowPriceVolume']
             if avgLow and avgHigh:
-                df.at[int(key), "avg Low diff"] = df.at[int(key), "low"] - avgLow
-                df.at[int(key), "avg high diff"] = df.at[int(key), "high"] - avgHigh
                 df.at[int(key), "1h volume"] = round(math.log(hvolume+1) * math.log(lvolume+1), 2)
+else:
+    print("r5 fail")
 
 r6 = requests.get(url6, headers=headers)
 
@@ -143,11 +162,9 @@ if r6.ok:
             hvolume = output[data][key]['highPriceVolume']
             lvolume = output[data][key]['lowPriceVolume']
             if avgLow and avgHigh:
-                df.at[int(key), "avg Low diff"] = df.at[int(key), "low"] - avgLow
-                df.at[int(key), "avg high diff"] = df.at[int(key), "high"] - avgHigh
                 df.at[int(key), "24h volume"] = round(math.log(hvolume+1) * math.log(lvolume+1), 2)
-
-
+else:
+    print("r6 fail")
 
 print(df)
 df.to_csv('out.csv', index=False)
